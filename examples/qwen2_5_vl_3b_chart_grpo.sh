ec495426aab69f121b38d6856203a0b75c0f5513
dragonlzm <89628732+dragonlzm@users.noreply.github.com>
2025-06-11 15:23:33 -0700

    update for the path

--- examples/qwen2_5_vl_3b_chart_grpo.sh
+++ examples/qwen2_5_vl_3b_chart_grpo.sh
@@ -7,13 +7,12 @@ export PYTHONUNBUFFERED=1
 MODEL_PATH=Qwen/Qwen2.5-VL-3B-Instruct  # replace it with your local file path
 
 python3 -m verl.trainer.main \
-    config=/home/ec2-user/generated_annotation/all_anno.json \
-    data.train_files=? \
+    config=examples/config.yaml \
+    data.train_files=/home/ec2-user/generated_annotation/all_anno.json \
     data.val_files=None \
     worker.actor.model.model_path=${MODEL_PATH} \
     trainer.experiment_name=qwen2_5_vl_3b_chart_grpo_testing \
     trainer.n_gpus_per_node=8 \
     data.use_self_dataset=True \
     data.format_prompt=./examples/format_prompt/chart_format.jinja \
-
-/home/ec2-user/updated_code_images
+    data.image_root=/home/ec2-user/updated_code_images
--- verl/trainer/config.py
+++ verl/trainer/config.py
@@ -50,7 +50,7 @@ class DataConfig:
     min_pixels: int = 262144
     filter_overlong_prompts: bool = True
     use_self_dataset: bool = False
-    image_root: str = None
+    image_root: str = '/home/ec2-user/updated_code_images'
 
     def post_init(self):
         if self.format_prompt is not None:
